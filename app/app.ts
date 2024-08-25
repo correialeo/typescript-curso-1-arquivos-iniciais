433d08841e2beceb03a4cec0826f3017f02305f1
correialeo <leandroo774@outlook.com>
2024-08-25 13:31:34 -0300

    Capítulo 1 e 2 curso 2 de TS. Generics<T, K> e Abstract.

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,6 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
+
 
 const controller = new NegociacaoController;
 const form = document.querySelector('.form');
--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,16 +1,21 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController{
     private inputData: HTMLInputElement;
     private inputQtd: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
 
     constructor(){
         this.inputData = document.querySelector('#data');
         this.inputQtd = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor')
+        this.negociacoesView.update(this.negociacoes);
     }
 
     criaNegociacao(): Negociacao{
@@ -36,6 +41,8 @@ export class NegociacaoController{
         negociacao.data.setDate(12)
         this.negociacoes.adiciona(negociacao);
         console.log(this.negociacoes.lista());
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso')
         this.limpaForm();
     }
 }
